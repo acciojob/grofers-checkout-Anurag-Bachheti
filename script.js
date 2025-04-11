f24705d1d068e2927f1ec6ca3070f9d6d495fa6c
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-11 21:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+let sumButton = document.createElement("button");
+sumButton.innerHTML = "Get Total Price"
+document.body.appendChild(sumButton);
 
 const getSum = () => {
-//Add your code here
-  
+	const priceAll = document.querySelectorAll(".price");
+	let total = 0;
+
+	for (let i = 0; i < priceAll.length; i++) {
+		const price = parseFloat(priceAll[i].textContent) || 0;
+		total += price;
+	}
+	document.getElementById("text").innerHTML = `${total}`
 };
 
-getSumBtn.addEventListener("click", getSum);
-
+sumButton.addEventListener("click", getSum);
